158d3bd2b3684645ddf50c862cc13461cad4fe85
Chiang Wen-Yun <jennychiang2016@wj-ar.com>
2022-08-18 15:11:10 +0800

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { HashRouter, NavLink, Routes, Route, useNavigate } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Index = () => {
+    return <p>首頁</p>
+}
+const Todo = () => {
+    const navigate = useNavigate()
+    return (
+        <>
+            <p>這是 Todo 頁面</p>
+            <button
+                onClick={() => {
+                    navigate('/login')
+                }}
+            >
+                Logout
+            </button>
+        </>
+    )
+}
+const Register = () => {
+    return <p>這是註冊頁面</p>
+}
+const Login = () => {
+    return <p>這是登入頁面</p>
 }
 
-export default App;
+function App() {
+    return (
+        <div className="container">
+            <HashRouter>
+                <div className="nav-link">
+                    <NavLink to="/">
+                        <p>回到首頁</p>
+                    </NavLink>
+                    <NavLink to="/register">
+                        <p>註冊頁面</p>
+                    </NavLink>
+                    <NavLink to="/login">
+                        <p>登入頁面</p>
+                    </NavLink>
+                    <NavLink to="/todo">
+                        <p>Todo 頁面</p>
+                    </NavLink>
+                </div>
+                {/* Routes, Route 練習區 */}
+                {/* 練習區 */}
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/todo" element={<Todo />} />
+                    <Route
+                        path="*"
+                        element={
+                            <main style={{ padding: '1rem' }}>
+                                <p>There's nothing here!</p>
+                            </main>
+                        }
+                    />
+                </Routes>
+            </HashRouter>
+        </div>
+    )
+}
+
+export default App
